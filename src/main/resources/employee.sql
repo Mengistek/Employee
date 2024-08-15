1556d5fe2a9a2256c54ed3f88db5c764f40a9922
Mengs Tekle <wakadj33@gmail.com>
2024-08-15 06:26:26 -0500

    create sql files

---
+++ src/main/resources/employee.sql
@@ -0,0 +1,11 @@
+INSERT INTO Employee (first_Name, last_Name, yearly_Salary)
+VALUES ('Daniel', 'Agar', 109980.00);
+
+INSERT INTO Employee (first_Name, last_Name, yearly_Salary)
+VALUES ('Benard', 'Shaw', 157750.00);
+
+INSERT INTO Employee (first_Name, last_Name, yearly_Salary)
+VALUES ('Carly', 'DeFiori', 142008.75);
+
+INSERT INTO Employee (first_Name, last_Name, yearly_Salary)
+VALUES ('Wesley', 'Schneider', 74500.00);
---
+++ src/main/resources/retirementPlan.sql
@@ -0,0 +1,11 @@
+INSERT INTO Retirement_Plan ( reference_Number, enrollment_Date, retirement_Date, monthly_Contribution, employee_id)
+VALUES ( 'EXU189', '2023-01-17', '2023-09-13', 100.00, 1);
+
+INSERT INTO Retirement_Plan ( reference_Number, enrollment_Date, retirement_Date, monthly_Contribution, employee_id)
+VALUES ( 'SMI104', '2023-02-20', '2023-09-21', 850.00, 2);
+
+INSERT INTO Retirement_Plan ( reference_Number, enrollment_Date, retirement_Date, monthly_Contribution, employee_id)
+VALUES ( 'SM2307', '2020-08-16', '2023-11-04', 1555.50, 3);
+
+INSERT INTO Retirement_Plan (reference_Number, enrollment_Date, retirement_Date, monthly_Contribution, employee_id)
+VALUES ( 'SM4115', '2019-12-01', '2023-06-30', 588.00, 4);
